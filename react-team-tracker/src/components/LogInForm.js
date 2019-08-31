3f090feb4fb56d1ae12f371f27b9c11d93f900f9
Miracleo Jean Sarmiento <mjeansarmiento@gmail.com>
2019-08-31 23:17:03 +0800

    [UPDATE] Add styling for LogInForm component
    * added new classNames

--- react-team-tracker/src/components/LogInForm.js
+++ react-team-tracker/src/components/LogInForm.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
+import { Form, Input, Icon, Button } from 'antd';
+import './LogInForm.css';
+import Title from 'antd/lib/typography/Title';
+import NavBarLink from './Header/NavBarLink';
 
 class LogInForm extends Component {
   state = {
@@ -52,11 +56,35 @@ class LogInForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" name="username" onChange={this.handleChange}/>
-        <input type="password" name="password" onChange={this.handleChange}/>
-        <button type="submit">Log In</button>
-      </form>
+      <div className="login-container">
+        <center>
+          <Title level={4}>Welcome!</Title>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+              <Input
+                name="username"
+                placeholder="Username"
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                onChange={this.handleChange}
+                required
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input.Password
+                name="password"
+                placeholder="Password"
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                onChange={this.handleChange}
+                required
+              />
+            </Form.Item>
+            <Button htmlType="submit" className="btn-submit">Log In</Button>
+            <div className="registration-link">
+              No account yet? <NavBarLink path="/register" label="Register now!"/>
+            </div>
+          </Form>
+        </center>
+      </div>
     );
   }
 }
